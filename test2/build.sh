314505df7df2eceb4565857ccdf496cc5d47e738
Peter Hall <peter.hall@digio.com.au>
2019-07-05 22:10:01 +1000

    Adding logic around tagging based on if PROJECT_ID

--- test2/build.sh
+++ test2/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 export COMMIT_SHA=$(git rev-parse HEAD)
 
+if [ -n $PROJECT_ID ]; then
+  TAG="gcr.io/${PROJECT_ID}/anz_test2:${COMMIT_SHA}"
+else
+  TAG="anz_test2:${COMMIT_SHA}"
+fi
+
 docker build --build-arg APP_VERSION=$(cat VERSION.txt) \
-    --build-arg COMMIT_SHA=$COMMIT_SHA -t anz_test2:$COMMIT_SHA .
+    --build-arg COMMIT_SHA=$COMMIT_SHA -t $TAG .
 
